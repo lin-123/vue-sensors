0fb10e81a13fc452b1feb3bbd86ec2296930b37d
houlinjiang <houlinjiang@jd.com>
2018-11-29 15:48:34 +0800

    fix vulnerability node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const webpack = require('webpack')
-const merge = require('webpack-merge')
 
 const webpackConfig = {
+  mode: process.env.NODE_ENV || 'development',
   entry: {
     app: './src/index.js',
   },
